cbd17745e04451b7ed17f46d8deb16ae8e0ac13b
rashmiwijemanna <rashmiwijemanna7@gmail.com>
2025-09-28 09:47:33 +0530

    add model package and book class & make propoties in Book class

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private String category;
+    private int availableCopies;
+}
