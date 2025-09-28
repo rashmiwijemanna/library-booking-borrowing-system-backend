a9ab3c70db76dfc6eb72139d3bfa77464e39de69
rashmiwijemanna <rashmiwijemanna7@gmail.com>
2025-09-28 12:22:50 +0530

    extends JpaRepository interface to BookRepository

---
+++ src/main/java/edu/icet/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package edu.icet.repository;
+
+import edu.icet.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+
+}
