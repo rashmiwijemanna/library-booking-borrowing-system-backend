20196444f41f0e7d425ba0a3220899c51d9b241c
rashmiwijemanna <rashmiwijemanna7@gmail.com>
2025-09-28 12:29:35 +0530

    put Autowired annotations in BookController & BookService classes

--- src/main/java/edu/icet/controller/BookController.java
+++ src/main/java/edu/icet/controller/BookController.java
@@ -1,6 +1,8 @@
 package edu.icet.controller;
 
+import edu.icet.service.BookService;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController // @Controller  @ResponseBody //retun json, xml type of values
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-
-
 public class BookController {
+    @Autowired
+    BookService bookService;
 
     @GetMapping
-    public String getName(){
-        return "blaa";
+    public void add(){
+        bookService.add();
+
+
+
+
     }
 
 
